e8540329412d6930b1d7739eed39b9af25e5237a
donkeeman <doonkeeemaan@gmail.com>
2025-03-23 14:35:14 +0900

    fix: 응답 형식 통일 및 인터셉터 추가

---
+++ server/src/common/interfaces/response.interface.ts
@@ -0,0 +1,13 @@
+export interface SuccessResponse<T> {
+    statusCode: number;
+    data: T;
+    message: string;
+}
+
+export interface ErrorResponse {
+    statusCode: number;
+    error: string;
+    message: string;
+}
+
+export type BaseResponse<T> = SuccessResponse<T> | ErrorResponse;
